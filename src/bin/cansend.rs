d0c37ae8f7dc9dff23fe3cac3ae14de4f15f5f2f
Raphael <raphael@nissl.net>
2021-02-09 13:52:28 +0100

    incremented version number of application

--- src/bin/cansend.rs
+++ src/bin/cansend.rs
@@ -108,7 +108,7 @@ fn test_frame_parsing_extended() {
 ///
 fn main() {
     let arg_matches = App::new("cansend")
-                            .version("0.1.0")
+                            .version("0.1.1")
                             .author("Raphael Nissl")
                             .about("Program sets a CAN-Frame on a bus with given ID and data (does not support CAN FD protocol)")
                             .arg(
